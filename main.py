053e980fb267de4def4597a5b4278f2efb3a3173
Helen <elena.kozovyakina@gmail.com>
2022-10-02 12:24:10 +0300

    oop_get_set

--- main.py
+++ main.py
@@ -3497,6 +3497,7 @@ import re
 
 import os
 
+
 # import os.path
 
 # print("Текущая директория:", os.getcwd())
@@ -4155,42 +4156,41 @@ import os
 # print(Change.inc(10), Change.dec(10))
 
 
-class Date:
-    def __init__(self, day=0, month=0, year=0):
-        self.day = day
-        self.month = month
-        self.year = year
-
-    def string_to_db(self):
-        return f'{self.year}-{self.month}-{self.day}'
-
-    @classmethod
-    def from_string(cls, data_as_string):
-        d, m, y = map(int, data_as_string.split('.'))
-        return cls(d, m, y)
-
-    @staticmethod
-    def is_date_valid(data_as_string):
-        if data_as_string.count('.') == 2:
-            d, m, y = map(int, data_as_string.split('.'))
-            return d <= 31 and m <= 12 and y <= 3999
-
-
-dates = [
-    '30.12.2020',
-    '30.12.2020',
-    '31.01.2021',
-    '12.31.2020'
-]
-
-for string_date in dates:
-    if Date.is_date_valid(string_date):
-        date = Date.from_string(string_date)  # '30.12.2020'
-        string_to_db = date.string_to_db()
-        print(string_to_db)
-    else:
-        print("Некорректная дата")
-
+# class Date:
+#     def __init__(self, day=0, month=0, year=0):
+#         self.day = day
+#         self.month = month
+#         self.year = year
+#
+#     def string_to_db(self):
+#         return f'{self.year}-{self.month}-{self.day}'
+#
+#     @classmethod
+#     def from_string(cls, data_as_string):
+#         d, m, y = map(int, data_as_string.split('.'))
+#         return cls(d, m, y)
+#
+#     @staticmethod
+#     def is_date_valid(data_as_string):
+#         if data_as_string.count('.') == 2:
+#             d, m, y = map(int, data_as_string.split('.'))
+#             return d <= 31 and m <= 12 and y <= 3999
+#
+#
+# dates = [
+#     '30.12.2020',
+#     '30.12.2020',
+#     '31.01.2021',
+#     '12.31.2020'
+# ]
+#
+# for string_date in dates:
+#     if Date.is_date_valid(string_date):
+#         date = Date.from_string(string_date)  # '30.12.2020'
+#         string_to_db = date.string_to_db()
+#         print(string_to_db)
+#     else:
+#         print("Некорректная дата")
 
 
 # d1 = Date.from_string('12.11.2022')
@@ -4203,3 +4203,180 @@ for string_date in dates:
 # d, m, y = map(int, string_date.split('.'))
 # date1 = Date(d, m, y)
 # print(date1.string_to_db())
+
+
+# class Weight:
+#     def __init__(self, k=0):
+#         self.__k = k
+#
+#     @property
+#     def kg_k(self):
+#         return self.__k
+#
+#     @kg_k.setter
+#     def kg_k(self, k):
+#         if isinstance(k, (int, float)):
+#             self.__k = k
+#         else:
+#             print("Килограммы задаются только числами")
+#
+#     def to_pounds(self):
+#         return round(self.__k * 2.205, 2)
+#
+#
+# w1 = Weight(12)
+# print(w1.kg_k, "кг => ", end="")
+# print(w1.to_pounds(), 'фунтов')
+#
+# w1.kg_k = 41.3
+# print(w1.kg_k, "кг => ", end="")
+# print(w1.to_pounds(), 'фунтов')
+#
+# w1.kg_k = 20
+# print(w1.kg_k, "кг => ", end="")
+# print(w1.to_pounds(), 'фунтов')
+
+
+# class Account:
+#     rate_usd = 0.013
+#     rate_eur = 0.011
+#     suffix = 'RUB'
+#     suffix_usd = 'USD'
+#     suffix_eur = 'EUR'
+#
+#     def __init__(self, num, surname, percent, value=0):
+#         self.num = num
+#         self.surname = surname
+#         self.percent = percent
+#         self.value = value
+#         print(f'Счет #{self.num} принадлежит {self.surname} был открыт.')
+#         print('*' * 50)
+#
+#     def __del__(self):
+#         print('*' * 50)
+#         print(f'Счет #{self.num} принадлежащий {self.surname} был закрыт.')
+#
+#     @classmethod
+#     def set_usd_rate(cls, rate):
+#         cls.rate_usd = rate
+#
+#     @classmethod
+#     def set_eur_rate(cls, rate):
+#         cls.rate_eur = rate
+#
+#     @staticmethod
+#     def convert(value, rate):
+#         return value * rate
+#
+#     def edit_owner(self, surname):
+#         self.surname = surname
+#
+#     def convert_to_usd(self):
+#         usd_val = Account.convert(self.value, Account.rate_usd)
+#         print(f'Состояние счета: {usd_val} {Account.suffix_usd}.')
+#
+#     def convert_to_eur(self):
+#         eur_val = Account.convert(self.value, Account.rate_eur)
+#         print(f'Состояние счета: {eur_val} {Account.suffix_eur}.')
+#
+#     def print_balance(self):
+#         print(f'Текущий баланс {self.value} {Account.suffix}')
+#
+#     def print_info(self):
+#         print(f'Информация о счете:')
+#         print('-' * 20)
+#         print(f'#{self.num}')
+#         print(f'Владелец: {self.surname}')
+#         self.print_balance()
+#         print(f'Проценты: {self.percent:.0%}')
+#         print('-' * 20)
+#
+#     def add_percents(self):
+#         self.value += self.value * self.percent
+#         print('Проценты были успешно начислены')
+#         self.print_balance()
+#
+#     def withdraw_money(self, val):
+#         if val > self.value:
+#             print(f'К сожалению у вас нет {val} {Account.suffix}')
+#         else:
+#             self.value -= val
+#             print(f'{val} {Account.suffix} было успешно снято!')
+#         self.print_balance()
+#
+#     def add_money(self, val):
+#         self.value += val
+#         print(f'{val} {Account.suffix} было успешно добавлено!')
+#         self.print_balance()
+#
+#
+# acc = Account('12345', 'Долгих', 0.03, 1000)
+# acc.print_info()
+# acc.convert_to_usd()
+# acc.convert_to_eur()
+# print()
+#
+# Account.set_usd_rate(2)
+# acc.convert_to_usd()
+#
+# Account.set_eur_rate(3)
+# acc.convert_to_eur()
+# print()
+#
+# acc.edit_owner('Дюма')
+# acc.print_info()
+# print()
+#
+# acc.add_percents()
+# print()
+#
+# acc.withdraw_money(3000)
+# print()
+#
+# acc.withdraw_money(100)
+# print()
+#
+# acc.add_money(5000)
+# print()
+#
+# acc.withdraw_money(3000)
+# print()
+import re
+
+
+class UserData:
+    def __init__(self, fio, old, ps, weight):
+        self.verify_fio(fio)
+        self.verify_old(old)
+        self.verify_weight(weight)
+
+        self.__fio = fio.split()
+        self.__old = old
+        self.__password = ps
+        self.__weight = weight
+
+    @classmethod
+    def verify_fio(cls, fio):
+        if not isinstance(fio, str):
+            raise TypeError("ФИО должно быть строкой")
+        f = fio.split()  # ['Волков', 'Игорь', 'Николаевич']
+        if len(f) != 3:
+            raise TypeError("Неверный формат ФИО")
+        letters = "".join(re.findall(r'[a-zа-яё-]', fio, flags=re.IGNORECASE))  # ВолковИгорьНиколаевич
+        # ['В', 'о', 'л', 'к', 'о', 'в', 'И', 'г', 'о', 'р', 'ь', 'Н', 'и', 'к', 'о', 'л', 'а', 'е', 'в', 'и', 'ч']
+        for s in f:
+            if len(s.strip(letters)) != 0:
+                raise TypeError("В ФИО можно использовать только буквы и дефис")
+
+    @classmethod
+    def verify_old(cls, old):
+        if not isinstance(old, int) or old < 14 or old > 120:
+            raise TypeError("Возраст должен быть числом в диапазоне от 14 до 120 лет")
+
+    @classmethod
+    def verify_weight(cls, w):
+        if not isinstance(w, float) or w < 20:
+            raise TypeError("Вес должен быть вещественным числом от 20 кг и выше")
+
+
+p1 = UserData("Волков Игорь Николаевич", 26, "1234 567890", 80.8)
