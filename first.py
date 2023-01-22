2b4dc511683a23f7b9fb2a1e21451e4e601efcad
Helen <elena.kozovyakina@gmail.com>
2023-01-22 12:36:09 +0300

    flask_db

--- first.py
+++ first.py
@@ -1,6 +1,31 @@
-from flask import Flask, render_template, url_for
+import sqlite3
+import os
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
+
+
+DATABASE = '/tmp/flsk.db'
+DEBUG = True
+SECRET_KEY = '1cc9eea42ab798cd406fe44c79652a60fbb008ef'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsk.db')))
+
+
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
 
 menu = [
     {"name": "Главная", "url": "index"},
@@ -24,7 +49,41 @@ def about():
 
 @app.route("/profile/<username>")
 def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
     return f"Пользователь: {username}"
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено успешно!', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+        # print(request.form)
+        # context = {
+        #     'username': request.form['username'],
+        #     'email': request.form['email'],
+        #     'message': request.form['message']
+        # }
+        # return render_template("contact.html", **context, title="Обратная связь", menu=menu)
+    return render_template("contact.html", title="Обратная связь", menu=menu)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'elena' and request.form['passw'] == "123456":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template("login.html", title="Авторизация", menu=menu)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("page404.html", title="Страница не найдена", menu=menu)
 
 
 if __name__ == "__main__":
